4f6ae1a382d60d6bdfe1c454f398adedee769274
Adam Baker <adam.m.baker@gmail.com>
2019-08-07 15:32:18 -0600

    Removed debug statements

--- intake/views/travelplan.py
+++ intake/views/travelplan.py
@@ -59,8 +59,6 @@ class TravelPlanCreateView(LoginRequiredMixin, CreateView):
             travel_mode = tp_travel_mode,
             notes = tp_notes,
         )
-        print('FAM',fam.id, fam.family_name)
-        print('TPP', tp, tp.id, tp_c)
         fam.travel_plan = tp
         fam.save()
         # return to parent detail
